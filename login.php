acb936179ab4ed0a66befb399dde9873f5716830
Christian Bartel <warwolfen@gmail.com>
2018-02-15 11:55:13 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,42 @@
-<?php
-  // Login
-
+<?php 
+  if(isset($_GET['login'])) {
+    $email = $_POST['email'];
+    $passwort = $_POST['passwort'];
+    
+    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $result = $statement->execute(array('email' => $email));
+    $user = $statement->fetch();
+        
+    //Überprüfung des Passworts
+    if ($user !== false && password_verify($passwort, $user['passwort'])) {
+        $_SESSION['userid'] = $user['id'];
+        die('Login erfolgreich. Weiter zu <a href="geheim.php">internen Bereich</a>');
+    } else {
+        $errorMessage = "E-Mail oder Passwort war ungültig<br>";
+    }
+}
 ?>
+
+<html> 
+<head>
+  <title>Login</title>    
+</head> 
+<body>
+ 
+<?php 
+if(isset($errorMessage)) {
+    echo $errorMessage;
+}
+?>
+ 
+<form action="?login=1" method="post">
+E-Mail:<br>
+<input type="email" size="40" maxlength="250" name="email"><br><br>
+ 
+Password:<br>
+<input type="password" size="40"  maxlength="250" name="passwort"><br>
+ 
+<input type="submit" value="Login">
+</form> 
+</body>
+</html>
