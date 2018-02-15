2b664ab4d06e64e340472f5a3bb731c1bb5ae6a8
Christian Bartel <warwolfen@gmail.com>
2018-02-15 12:01:15 +0100

    Update unitselector.php

--- unitselector.php
+++ unitselector.php
@@ -7,6 +7,16 @@
 <head>
 	<link rel="stylesheet" href="./styles/styles.css" type="text/css">
 	<link rel="icon" href="./favicon.png" type="image/png">
+	<link rel="shortcut icon" href="./images/icon_196x196.png" type="image/png" sizes="196x196">
+	<link rel="apple-touch-icon" href="./images/icon_57x57.png" type="image/png" sizes="57x57">
+	<link rel="apple-touch-icon" href="./images/icon_72x72.png" type="image/png" sizes="72x72">
+	<link rel="apple-touch-icon" href="./images/icon_76x76.png" type="image/png" sizes="76x76">
+	<link rel="apple-touch-icon" href="./images/icon_114x114.png" type="image/png" sizes="114x114">
+	<link rel="apple-touch-icon" href="./images/icon_120x120.png" type="image/png" sizes="120x120">
+	<link rel="apple-touch-icon" href="./images/icon_144x144.png" type="image/png" sizes="144x144">
+	<link rel="apple-touch-icon" href="./images/icon_152x152.png" type="image/png" sizes="152x152">
+	<link rel="apple-touch-icon" href="./images/icon_180x180.png" type="image/png" sizes="180x180">
+
 	<style>
 		html, body {
 			background-image: url('./images/body-bg_2.png');
