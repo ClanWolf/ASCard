64484c37886e27c5aeb6a76cadbd275ee01126ac
Christian Bartel <warwolfen@gmail.com>
2019-03-08 12:46:01 +0100

    Update gui_assignunit.php

--- gui_assignunit.php
+++ gui_assignunit.php
@@ -11,6 +11,116 @@ session_start();
 	$pid = $_SESSION['playerid'];
 	$pname = $_SESSION['name'];
 	$pimage = $_SESSION['playerimage'];
-
-
 ?>
+
+<html lang="en">
+
+<head>
+	<title>ClanWolf.net: AplhaStrike Card App (ASCard): Unit creator</title>
+	<meta charset="utf-8">
+	<meta http-equiv="expires" content="0">
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
+	<meta name="description" content="Cards app for the AlphaStrike TableTop (BattleTech).">
+	<meta name="keywords" content="BattleTech, AlphaStrike, Mech">
+	<meta name="robots" content="noindex,nofollow">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name='viewport' content='user-scalable=0'>
+
+	<link rel="manifest" href="./manifest.json">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="./styles/styles.css">
+	<link rel="icon" href="./favicon.png" type="image/png">
+	<link rel="shortcut icon" href="./images/icon_196x196.png" type="image/png" sizes="196x196">
+	<link rel="apple-touch-icon" href="./images/icon_57x57.png" type="image/png" sizes="57x57">
+	<link rel="apple-touch-icon" href="./images/icon_72x72.png" type="image/png" sizes="72x72">
+	<link rel="apple-touch-icon" href="./images/icon_76x76.png" type="image/png" sizes="76x76">
+	<link rel="apple-touch-icon" href="./images/icon_114x114.png" type="image/png" sizes="114x114">
+	<link rel="apple-touch-icon" href="./images/icon_120x120.png" type="image/png" sizes="120x120">
+	<link rel="apple-touch-icon" href="./images/icon_144x144.png" type="image/png" sizes="144x144">
+	<link rel="apple-touch-icon" href="./images/icon_152x152.png" type="image/png" sizes="152x152">
+	<link rel="apple-touch-icon" href="./images/icon_180x180.png" type="image/png" sizes="180x180">
+
+	<script type="text/javascript" src="./scripts/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="./scripts/basic.js"></script>
+	<script type="text/javascript" src="./scripts/masterunitlist.js"></script>
+
+	<style>
+		html, body {
+			background-image: url('./images/body-bg_2.jpg');
+		}
+		table {
+			margin-left: auto;
+			margin-right: auto;
+		}
+		.box {
+			width: 80%;
+			background-color :#transparent;
+			top: 50%;
+			left: 50%;
+		}
+		.options {
+			border-radius: 5px;
+			border-style: solid;
+			border-width: 3px;
+			padding: 5px;
+			background: rgba(60,60,60,0.75);
+			color: #ddd;
+			border-color: #aaa;
+		}
+		input, select {
+			width: 80px;
+			vertical-align: middle;
+			color: #ddd;
+			border-width: 0px;
+			padding: 2px;
+			font-family: 'Pathway Gothic One', sans-serif;
+		}
+		select:focus, textarea:focus, input:focus {
+			outline: none;
+		}
+		select:invalid, input:invalid {
+			background: rgba(40,40,40,0.75);;
+		}
+		select:valid, input:valid {
+			background: rgba(70,70,70,0.75);;
+		}
+	</style>
+</head>
+
+<body>
+	<script>
+		$(document).ready(function() {
+			$("#cover").hide();
+			fetchMechList();
+			document.getElementById("units").selectedIndex = "1";
+		});
+	</script>
+<?php
+	echo "<div id='player_image'>\n";
+	echo "	<img src='./images/player/".$pimage."' width='60px' height='60px'>\n";
+	echo "</div>\n";
+?>
+	<div id="cover"></div>
+	<div id="header">
+		<table style="width:100%;height:60px;border:none;border-collapse:collapse;background:rgba(50,50,50,1.0);" cellspacing="0" cellpadding="0">
+			<tr>
+				<td nowrap onclick="location.href='./logout.php'" width="60px" style="background: rgba(50,50,50,1.0); text-align: center; vertical-align: middle;">
+					<div><a style="color: #eee;" href="./logout.php"><i class="fa fa-power-off" aria-hidden="true"></i></a></div>
+				</td>
+				<td nowrap onclick="location.href='./gui_selectunit.php'" width="20%"><div class='mechselect_button_normal'><a href='./gui_selectunit.php'>ROSTER</a><br><span style='font-size:16px;'>Choose a unit to play</span></div></td>
+				<td nowrap onclick="location.href='./gui_enemies.php'" width="20%"><div class='mechselect_button_normal'><a href='./gui_enemies.php'>OPFOR</a><br><span style='font-size:16px;'>Enemy Mechs</span></div></td>
+				<td nowrap onclick="location.href='./gui_createunit.php'" width="20%"><div class='mechselect_button_active'><a href='./gui_createunit.php'>ADD MECH</a><br><span style='font-size:16px;'>Create a new unit and pilot</span></div></td>
+				<td nowrap onclick="location.href='./gui_createplayer.php'" width="20%"><div class='mechselect_button_normal'><a href='./gui_createplayer.php'>PLAYER</a><br><span style='font-size:16px;'>Manage players</span></div></td>
+				<td nowrap onclick="location.href='./gui_options.php'" width="20%"><div class='mechselect_button_normal'><a href='./gui_options.php'>OPTIONS</a><br><span style='font-size:16px;'>Change options</span></div></td>
+				<td nowrap width="60px" style="background: rgba(50,50,50,1.0); text-align: center; vertical-align: middle;"><div id='loggedOnUser'></div></td>
+			</tr>
+		</table>
+	</div>
+
+	<br>
+
+</body>
+
+</html>
