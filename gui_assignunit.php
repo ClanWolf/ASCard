78fa8d2cb5726101b26537b984bdf3268958a09b
Christian Bartel <warwolfen@gmail.com>
2019-03-08 12:35:48 +0100

    Update gui_assignunit.php

--- gui_assignunit.php
+++ gui_assignunit.php
@@ -1,1 +1,16 @@
-dummy
+<?php
+session_start();
+// https://www.php-einfach.de/php-tutorial/php-sessions/
+	require('./db.php');
+	if (!isset($_SESSION['playerid'])) {
+		echo "Not logged in... redirecting.<br>";
+		echo "<meta http-equiv='refresh' content='0;url=./login.php'>";
+		die();
+	}
+	// Get data on units from db
+	$pid = $_SESSION['playerid'];
+	$pname = $_SESSION['name'];
+	$pimage = $_SESSION['playerimage'];
+
+
+?>
