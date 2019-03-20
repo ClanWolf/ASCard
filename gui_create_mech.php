ab7dc90c5fd6db9b97ab2caa2bf8622e55886661
Christian Bartel <warwolfen@gmail.com>
2019-03-20 13:57:27 +0100

    Update gui_create_mech.php

--- gui_create_mech.php
+++ gui_create_mech.php
@@ -519,9 +519,11 @@ session_start();
 						<option value="0">0</option>
 						<option value="1">1</option>
 						<option value="2">2</option>
-						<option value="3" selected>3</option>
-						<option value="4">4</option>
+						<option value="3">3</option>
+						<option value="4" selected>4</option>
 						<option value="5">5</option>
+						<option value="6">6</option>
+						<option value="7">7</option>
 					</select>
 				</td>
 			</tr>
