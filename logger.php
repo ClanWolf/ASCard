532423e4c8569adde44279d421d4b0949bae539d
Christian Bartel <warwolfen@gmail.com>
2019-03-21 11:59:45 +0100

    Update logger.php

--- logger.php
+++ logger.php
@@ -1,7 +1,23 @@
 <?php
-
-	function log() {
-		
+	// write a line to log
+	function logMsg(msg) {
+		$logdatei=fopen("logs/logfile.txt","a");
+		fputs($logdatei,
+		      date("d.m.Y, H:i:s",time()) .
+		      msg ."\n"
+		);
+		fclose($logdatei);
 	}
 
+	// log the access to a file
+	$logdatei=fopen("logs/logfile.txt","a");
+	fputs($logdatei,
+		date("d.m.Y, H:i:s",time()) .
+		", " . $_SERVER['REMOTE_ADDR'] .
+		", " . $_SERVER['REQUEST_METHOD'] .
+		", " . $_SERVER['PHP_SELF'] .
+		", " . $_SERVER['HTTP_USER_AGENT'] .
+		", " . $_SERVER['HTTP_REFERER'] ."\n"
+	);
+	fclose($logdatei);
 ?>
