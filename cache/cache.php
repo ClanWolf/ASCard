ec037575607ced6ae43f6d361d020cb378916da5
Christian Bartel <warwolfen@gmail.com>
2020-08-27 20:26:02 +0200

    Update cache.php

--- cache/cache.php
+++ cache/cache.php
@@ -1,4 +1,10 @@
 <?php
-  $response = file_get_contents('https://www.clanwolf.net');
-  echo $response;
+  $Clan_0-2 = file_get_contents('https://www.clanwolf.net/apps/ASCard/cache/cache.php');
+  echo $Clan_0-2;
+
+  $filename = "test.json";
+  $fileHandle = fopen($filename, 'w');
+  fwrite($fileHandle, $Clan_0-2);
+
+  fclose($fileHandle);
 ?>
