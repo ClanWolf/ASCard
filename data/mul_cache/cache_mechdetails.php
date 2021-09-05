e917bb54eb5b709bf0da304bea4e6d6ebc6b47c1
WarWolfen <warwolfen@gmail.com>
2021-09-05 13:44:02 +0200

    Creating and deleting players.

--- data/mul_cache/cache_mechdetails.php
+++ data/mul_cache/cache_mechdetails.php
@@ -1,36 +1,3 @@
-<?php
-	echo "ASCard App - caching mech details from MUL<br>";
-	echo "-------------------------------------------<br>";
-	echo date('M Y') . "<br>";
-	echo "-------------------------------------------<br>";
-	echo "<br>";
-
-	$opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
-	$context = stream_context_create($opts);
-
-	for ($i = 9600; $i <= 9800; $i++ ) {
-        $url_value = "http://www.masterunitlist.info/Unit/QuickDetails?id=" . $i;
-		$content = file_get_contents($url_value, false, $context);
-
-		echo $url_value . ": ";
-
-		if ($content === FALSE) {
-			echo "Error while saving.";
-			echo "<br>";
-		} else {
-			echo "Saving " . $filename_arr[$key] . "... ";
-			// echo $content;
-			$filename = "/var/www/vhosts/clanwolf.net/httpdocs/apps/ASCard/cache/mul/mechdetails/" . $i . ".json";
-			$fileHandle = fopen($filename, 'w');
-			fwrite($fileHandle, $content);
-			fclose($fileHandle);
-			echo "saved.<br>";
-		}
-	}
-?>
-
-// https://www.clanwolf.net/path/cache_mechdetails.php?startindex=100
-
 //<?php
 //	echo "ASCard App - caching mech details from MUL<br>";
 //	echo "-------------------------------------------<br>";
@@ -41,8 +8,7 @@
 //	$opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
 //	$context = stream_context_create($opts);
 //
-//    $index = intval($_GET["startindex"]);
-//	for ($i = $index; $i <= $index+200; $i++ ) {
+//	for ($i = 9600; $i <= 9800; $i++ ) {
 //        $url_value = "http://www.masterunitlist.info/Unit/QuickDetails?id=" . $i;
 //		$content = file_get_contents($url_value, false, $context);
 //
@@ -62,3 +28,37 @@
 //		}
 //	}
 //?>
+
+// https://www.clanwolf.net/path/cache_mechdetails.php?startindex=100
+
+<?php
+	echo "ASCard App - caching mech details from MUL<br>";
+	echo "-------------------------------------------<br>";
+	echo date('M Y') . "<br>";
+	echo "-------------------------------------------<br>";
+	echo "<br>";
+
+	$opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
+	$context = stream_context_create($opts);
+
+    $index = intval($_GET["startindex"]);
+	for ($i = $index; $i <= $index+200; $i++ ) {
+        $url_value = "http://www.masterunitlist.info/Unit/QuickDetails?id=" . $i;
+		$content = file_get_contents($url_value, false, $context);
+
+		echo $url_value . ": ";
+
+		if ($content === FALSE) {
+			echo "Error while saving.";
+			echo "<br>";
+		} else {
+			echo "Saving " . $filename_arr[$key] . "... ";
+			// echo $content;
+			$filename = "/var/www/vhosts/clanwolf.net/httpdocs/apps/ASCard/cache/mul/mechdetails/" . $i . ".json";
+			$fileHandle = fopen($filename, 'w');
+			fwrite($fileHandle, $content);
+			fclose($fileHandle);
+			echo "saved.<br>";
+		}
+	}
+?>
