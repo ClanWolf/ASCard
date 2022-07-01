55b8c38e7dc2d165c289cfdebf9e26004a6bdd7e
WarWolfen <warwolfen@gmail.com>
2022-07-01 15:15:58 +0200

    Fixes after clan conclave 2022.

--- login.php
+++ login.php
@@ -81,8 +81,8 @@ session_start();
 	<meta name='viewport' content='user-scalable=0'>
 
 	<link rel="manifest" href="./manifest.json">
-	<link rel="stylesheet" href="./styles/styles.css" type="text/css">
-	<link rel="stylesheet" href="./styles/cookieconsent.css>" media="print" onload="this.media='all'">
+	<link rel="stylesheet" type="text/css" href="./styles/styles.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="./styles/cookieconsent.css" />
 	<link rel="icon" href="./favicon.png" type="image/png">
 	<link rel="shortcut icon" href="./images/icon_196x196.png" type="image/png" sizes="196x196">
 	<link rel="apple-touch-icon" href="./images/icon_57x57.png" type="image/png" sizes="57x57">
@@ -124,141 +124,27 @@ session_start();
 </head>
 
 <body>
-	<script src="./scripts/cookieconsent.js"></script>
-
-	<script>
-//        window.addEventListener('load', function(){
-//
-//            // obtain plugin
-//            var cc = initCookieConsent();
-//
-//            // run plugin with your configuration
-//            cc.run({
-//                current_lang: 'en',
-//                autoclear_cookies: false,                   // default: false
-//                page_scripts: false,                        // default: false
-//
-//                mode: 'opt-in',                          // default: 'opt-in'; value: 'opt-in' or 'opt-out'
-//                // delay: 0,                               // default: 0
-//                // auto_language: '',                      // default: null; could also be 'browser' or 'document'
-//                autorun: true,                          // default: true
-//                // force_consent: false,                   // default: false
-//                // hide_from_bots: false,                  // default: false
-//                // remove_cookie_tables: false             // default: false
-//                // cookie_name: 'cc_cookie',               // default: 'cc_cookie'
-//                // cookie_expiration: 182,                 // default: 182 (days)
-//                // cookie_necessary_only_expiration: 182   // default: disabled
-//                // cookie_domain: location.hostname,       // default: current domain
-//                // cookie_path: '/',                       // default: root
-//                // cookie_same_site: 'Lax',                // default: 'Lax'
-//                // use_rfc_cookie: false,                  // default: false
-//                revision: 0,                            // default: 0
-//
-//                onFirstAction: function(user_preferences, cookie){
-//                    // callback triggered only once on the first accept/reject action
-//                },
-//
-//                onAccept: function (cookie) {
-//                    // callback triggered on the first accept/reject action, and after each page load
-//                },
-//
-//                onChange: function (cookie, changed_categories) {
-//                    // callback triggered when user changes preferences after consent has already been given
-//                },
-//
-//    gui_options: {
-//        consent_modal: {
-//            layout: 'cloud',               // box/cloud/bar
-//            position: 'top center',     // bottom/middle/top + left/right/center
-//            transition: 'slide',           // zoom/slide
-//            swap_buttons: false            // enable to invert buttons
-//        },
-//        settings_modal: {
-//            layout: 'box',                 // box/bar
-//            // position: 'left',           // left/right
-//            transition: 'slide'            // zoom/slide
-//        }
-//    },
-//
-//                languages: {
-//                    'en': {
-//                        consent_modal: {
-//                            title: 'We use cookies!',
-//                            description: 'Hi, this website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
-//                            primary_btn: {
-//                                text: 'Accept all',
-//                                role: 'accept_all'              // 'accept_selected' or 'accept_all'
-//                            },
-//                            secondary_btn: {
-//                                text: 'Reject all',
-//                                role: 'accept_necessary'        // 'settings' or 'accept_necessary'
-//                            }
-//                        },
-//                        settings_modal: {
-//                            title: 'Cookie preferences',
-//                            save_settings_btn: 'Save settings',
-//                            accept_all_btn: 'Accept all',
-//                            reject_all_btn: 'Reject all',
-//                            close_btn_label: 'Close',
-//                            cookie_table_headers: [
-//                                {col1: 'Name'},
-//                                {col2: 'Domain'},
-//                                {col3: 'Expiration'},
-//                                {col4: 'Description'}
-//                            ],
-//                            blocks: [
-//                                {
-//                                    title: 'Cookie usage 📢',
-//                                    description: 'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="#" class="cc-link">privacy policy</a>.'
-//                                }, {
-//                                    title: 'Strictly necessary cookies',
-//                                    description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly',
-//                                    toggle: {
-//                                        value: 'necessary',
-//                                        enabled: true,
-//                                        readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
-//                                    }
-//                                }, {
-//                                    title: 'Performance and Analytics cookies',
-//                                    description: 'These cookies allow the website to remember the choices you have made in the past',
-//                                    toggle: {
-//                                        value: 'analytics',     // your cookie category
-//                                        enabled: false,
-//                                        readonly: false
-//                                    },
-//                                    cookie_table: [             // list of all expected cookies
-//                                        {
-//                                            col1: '^_ga',       // match all cookies starting with "_ga"
-//                                            col2: 'google.com',
-//                                            col3: '2 years',
-//                                            col4: 'description ...',
-//                                            is_regex: true
-//                                        },
-//                                        {
-//                                            col1: '_gid',
-//                                            col2: 'google.com',
-//                                            col3: '1 day',
-//                                            col4: 'description ...',
-//                                        }
-//                                    ]
-//                                }, {
-//                                    title: 'Advertisement and Targeting cookies',
-//                                    description: 'These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you',
-//                                    toggle: {
-//                                        value: 'targeting',
-//                                        enabled: false,
-//                                        readonly: false
-//                                    }
-//                                }, {
-//                                    title: 'More information',
-//                                    description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="#yourcontactpage">contact us</a>.',
-//                                }
-//                            ]
-//                        }
-//                    }
-//                }
-//            });
-//        });
+    <script type="text/javascript" src="./scripts/cookieconsent.js"></script>
+    <script>
+		window.addEventListener("load", function(){
+			window.cookieconsent.initialise({
+				"palette": {
+					"popup": {
+						"background": "#216942",
+						"text": "#b2d192"
+					},
+					"button": {
+						"background": "#afed71"
+					}
+				},
+				"position": "top",
+				"content": {
+				"message": "This app uses cookies to store options and values. No personal data is stored. To use the app, you need to confirm (DSGVO 2022).",
+				"dismiss": "Confirm",
+				"link": "Learn more..."
+				}
+			})
+		});
 
 		$(document).ready(function() {
 			document.getElementById("f1").style.visibility = "visible";
