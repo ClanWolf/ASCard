3a03a1d52b05854d4d9f3ec6bac9850b7c2677f5
Christian Bartel <warwolfen@gmail.com>
2022-11-22 16:48:23 +0100

    Update gui_show_specialabilities.php

--- gui_show_specialabilities.php
+++ gui_show_specialabilities.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
 
+<!-- https://spikeybits.com/2017/04/more-special-abilities-you-need-to-know-for-alpha-strike.html -->
+
 <head>
 	<title>ClanWolf.net: AplhaStrike Card App (ASCard): Special abilities</title>
 	<meta charset="utf-8">
