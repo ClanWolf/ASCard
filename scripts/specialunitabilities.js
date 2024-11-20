060d108ff4c9a4de90770801074c35af8a9f0b8e
Christian Bartel <warwolfen@gmail.com>
2024-11-20 15:07:34 +0100

    Update specialunitabilities.js

--- scripts/specialunitabilities.js
+++ scripts/specialunitabilities.js
@@ -1,1 +1,18 @@
-Read special abilities from json file
+// Read special abilities from json file
+
+function getSpecialAbilities() {
+	$.getJSON(cache_url, function (json) {
+		json.SpecialAbilities.sort(function(a, b) {
+		    if (a.Name < b.Name) return -1;
+			if (a.Name > b.Name) return 1;
+		});
+		$.each(json.SpecialAbilities, function (i, specialAbility) {
+			var name = specialAbility.name;
+
+		  console.log(name);
+
+		});
+	}).then(function data() {
+		document.getElementById("units").innerHTML = optionList;
+	});
+}
