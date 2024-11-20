c974df4d8987542afb84d033f9cde28d5ae709ef
Christian Bartel <warwolfen@gmail.com>
2024-11-20 15:10:23 +0100

    Update specialunitabilities.js

--- scripts/specialunitabilities.js
+++ scripts/specialunitabilities.js
@@ -1,16 +1,16 @@
 // Read special abilities from json file
 
-function getSpecialAbilities() {
+function getSpecialUnitAbilities() {
+	var cache_url = 'https://www.clanwolf.net/apps/ASCard/data/specialabilities/specialunitabilities.json';
+
 	$.getJSON(cache_url, function (json) {
 		json.SpecialAbilities.sort(function(a, b) {
-		    if (a.Name < b.Name) return -1;
+			if (a.Name < b.Name) return -1;
 			if (a.Name > b.Name) return 1;
 		});
 		$.each(json.SpecialAbilities, function (i, specialAbility) {
 			var name = specialAbility.name;
-
-		  console.log(name);
-
+			console.log(name);
 		});
 	}).then(function data() {
 		document.getElementById("units").innerHTML = optionList;
