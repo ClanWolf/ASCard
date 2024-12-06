d23870835abc970e753bdc653851fb3a6978dcb2
WarWolfen <warwolfen@gmail.com>
2024-12-06 13:16:32 +0100

    Vehicles

--- save_reset_round.php
+++ save_reset_round.php
@@ -1,6 +1,10 @@
 <?php
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
 	session_start();
-	ini_set("display_errors", 1); error_reporting(E_ALL);
 
 	require('./logger.php');
 	require_once('./db.php');
